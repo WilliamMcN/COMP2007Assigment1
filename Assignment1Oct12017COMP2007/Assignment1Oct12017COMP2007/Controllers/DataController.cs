9db9455ea198ca6a70c28cd193cb0f67c695a743
Fir3AtWill <Fir3AtWill@DESKTOP-94C6DB6>
2017-10-04 04:53:51 -0400

    Third Commit, Added some comments and also updated the read me file to tell user on what my webpage does and added the bonus

---
+++ Assignment1Oct12017COMP2007/Assignment1Oct12017COMP2007/Controllers/DataController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class DataController : Controller
+    {
+        //Tells the site to wait 10 seconds before next update 
+        [OutputCache(Duration = 10)]
+        public ActionResult Index()
+        {
+            //Creating a viewbag time to display the time, "T" means only show the time part not the date and time 
+            ViewBag.Time = DateTime.Now.ToString("T");
+            return View();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # COMP2007Assigment1
 Creating webpage to display datebase information
+What my project is, is a party finder web app, what you will be able to do is view parties in certain
+location based on the city you selected. When you selected a city it will show the current parties, the breif message,
+the type of party, and address of that party
+
+Bonus:
+I added a time feature that display time every ten seconds in the webpage using web.mcv, to view time click on the time button in the nav bar 
